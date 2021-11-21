70e1396fa1050e8a9098d978e483db71578460e9
Achmad Muzaki G <85576745+Zaki140502@users.noreply.github.com>
2021-11-21 23:19:03 +0700

    membuat migration tabel employee (#3)
    * membuat migration tabel employee
    
    * benerin table role
    
    * ubah nama tabel jadi employees

---
+++ database/migrations/2021_11_21_153529_employee.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Employee extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('employee_code');
+            $table->string('employee_fullname');
+            $table->string('phone_number');
+            $table->text('address');
+            $table->enum('role', ['Staff', 'Manager', 'Owner', 'Admin']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
