66f60c9e6e4464d350956d6523459a54bc70bbe6
Denis Aji <200605110003@student.uin-malang.ac.id>
2021-11-23 13:41:19 +0700

    Membuat feature Costumer(migration)

---
+++ app/Http/Controllers/CostumerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Costumer;
+use Illuminate\Http\Request;
+
+class CostumerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Costumer  $costumer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Costumer $costumer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Costumer  $costumer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Costumer $costumer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Costumer  $costumer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Costumer $costumer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Costumer  $costumer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Costumer $costumer)
+    {
+        //
+    }
+}
