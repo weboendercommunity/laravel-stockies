e6ce97f28eedeee297e907a2d3124762cb7ab260
[Firyal Nabila] <[firyalnabila02@gmail.com]>
2021-11-29 13:59:13 +0700

    revisi layoutbase

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,8 +7,6 @@
 
     <title>Laravel</title>
 
-    @yield('content')
-
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
@@ -38,29 +36,10 @@
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-
-                    @yield('content')
-
+                    
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                    </div>
-
-                    <div class="row">
-                        <div class="col">
-                            <div class="card">
-                                <div class="card-header">
-                                    <div class="card-title">
-                                        <h3>Hellow World!</h3>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <p>This is the beginning of Laravel Stockies</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                    @yield('content')
+                    
                 </div>
                 <!-- /.container-fluid -->
 
